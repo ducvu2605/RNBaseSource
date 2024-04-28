b5953c94fe733450c0a1674b00857627b113bc0a
ĐVũ <nguyenducvu26051999@gmail.com>
2024-04-28 16:01:16 +0700

    Setup Socket-IO v4

--- src/services/network/axios/index.ts
+++ src/services/network/axios/index.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import {BASE_URL, TIMEOUT} from './index.constant';
+import {BASE_URL, TIMEOUT} from './index.constants';
 import {
   reponseInterceptorFulfilled,
   reponseInterceptorReject,
